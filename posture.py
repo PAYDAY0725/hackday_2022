f06f53f0827b9314cbc8e1f8335e8765d0a864ba
Masashi <m.k_payday@outlook.com>
2022-09-17 17:15:08 +0900

    posture_date

--- posture.py
+++ posture.py
@@ -6,10 +6,13 @@
 
 import wiringpi
 
-def posture(plug):
-    wiringpi.wiringPiSetupGpio()
-    wiringpi.pinMode(25,1)
-    
-    if plug == 1:
-        wiringpi.digitalWrite(25,1)
+class piras:
 
+    def posture(plug):
+        wiringpi.wiringPiSetupGpio()
+        wiringpi.pinMode(25,1)
+
+        if plug == 1:
+            wiringpi.digitalWrite(25,1)
+        else:
+            wiringpi.digitalWrite(25,0)
